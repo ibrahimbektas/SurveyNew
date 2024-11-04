b3d6ba69477aa16dbec83785ab587050e5e2d7f7
İbrahim BEKTAŞ <brahim BEKTA@IBRAHIMBEKTAS>
2024-11-04 16:50:32 +0300

    Creator IdentityUser olarak tanımlandı. İlişki ayarlama öncesi

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete
 {
-    public class Context:IdentityDbContext
+    public class Context:IdentityDbContext<Creator, CreatorRole , int>
     {
         //public Context(DbContextOptions<Context> options) : base(options)
         //{
@@ -25,7 +25,6 @@ namespace DataAccessLayer.Concrete
         public DbSet<Survey> Surveys { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<ConditionalQuestion> ConditionalQuestions { get; set; } 
-        public DbSet<Creator> Creators { get; set; }
         public DbSet<UploadedFile> UploadedFiles { get; set; }
         public DbSet<Option> Options { get; set; }
         public DbSet<Response> Responses { get; set; }
